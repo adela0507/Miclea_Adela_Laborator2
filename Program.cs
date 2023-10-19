27e270a0186b641c6dc734354d2f8181402f0340
adela miclea <adelamiclea709@yahoo.com>
2023-10-19 19:44:35 +0300

    wow

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
+using Miclea_Adela_Laborator2.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<LibraryContext>(options =>
+
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
-
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    DbInitializer.Initialize(services);
+}
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
