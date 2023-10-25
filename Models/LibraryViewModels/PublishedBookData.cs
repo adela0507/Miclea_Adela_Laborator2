20ba4bed678f73ec3c98c3d1f5664df29c4d9154
adela miclea <adelamiclea709@yahoo.com>
2023-10-25 18:47:17 +0300

    done

---
+++ Models/LibraryViewModels/PublishedBookData.cs
@@ -0,0 +1,11 @@
+﻿namespace Miclea_Adela_Laborator2.Models.LibraryViewModels
+{
+    public class PublishedBookData
+    {
+        
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public bool IsPublished { get; set; }
+
+    }
+}
